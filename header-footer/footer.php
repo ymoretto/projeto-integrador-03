dfdad3d35bd0d88c10855d0e04a6d1121f0e03c4
dyonatanlfs <dyonatan55@hotmail.com>
2019-10-17 23:29:37 -0300

    Arquivos header e footer.

---
+++ header-footer/footer.php
@@ -0,0 +1,14 @@
+<footer class="container-fluid"> 	
+	<ul class="nav" id="listafooter">
+		<li id="paglista" class="col order-1 nav-item"><a href="#">Home</a></li>
+		<li id="paglista" class="col order-2 nav-item"><a href="#">Sobre nós</a></li>
+		<li id="paglista" class="col order-3 nav-item"><a href="#">Faça parte</a></li>
+		<li id="paglista" class="col order-4 nav-item"><a href="#">Login</a></li>
+	</ul>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzoXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
+</footer>
---
+++ header-footer/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+	<meta charset="UTF-8">
+    <title>Colaki</title>
+    <link rel="shortcut icon" href="img/Colaki-logo(1).png"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Chilanka|Open+Sans&display=swap" rel="stylesheet"> 
+  	<link href="http://code.ionicframework.com/ionicons/1.5.2/css/ionicons.min.css" rel="stylesheet">
+  	<script src="//code.jquery.com/jquery-2.1.0.min.js" type="text/javascript"></script>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+	<header class="container navbar .navbar-expand-lg sticky-top navbar-light bg-light">	
+			<div class="col-3">		
+				<a href="#" id="logo"><img src="img/Colaki-logo(3).png" width=120px></a>
+			</div>		
+			<nav class="col-9 container">					
+				<ol class="row nav col-10" id="lista">
+					<li class="col nav-item"><a class="nav-link active" href="#">Home</a></li>
+					<li class="col nav-item"><a class="nav-link" href="#">Sobre nós</a></li>
+					<li class="col nav-item"><a class="nav-link" href="#">Faça parte</a></li>
+					<li class="col nav-item"><a class="nav-link" href="#">Login</a></li>
+					<a href="#" id="menu" >☰</a>
+				</ol>
+				
+			</nav>
+
+	</header> 
+
