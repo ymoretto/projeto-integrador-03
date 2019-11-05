a2ea5cc4afa59c88c53f33dad70f796cd30a8b3e
dyonatanlfs <dyonatan55@hotmail.com>
2019-11-05 20:05:44 -0200

    Inserção de estilo html e body em um único css. Inserção de header e footer na página Login.

--- header.php
+++ header.php
@@ -1,19 +1,23 @@
 <!DOCTYPE html>
 <html lang="pt">
+
 <head>
-	<meta charset="UTF-8">
-    <title>Colaki</title>
-    <link rel="shortcut icon" href="images/colaki-logo-icone-verde.png">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Chilanka|Open+Sans&display=swap" rel="stylesheet"> 
-  	<link href="http://code.ionicframework.com/ionicons/1.5.2/css/ionicons.min.css" rel="stylesheet">
-  	<script src="//code.jquery.com/jquery-2.1.0.min.js" type="text/javascript"></script>
-    <link rel="stylesheet" href="css/style-home.css">
+  <meta charset="UTF-8">
+  <title>Colaki</title>
+  <link rel="shortcut icon" href="images/colaki-logo-icone-verde.png">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Chilanka|Open+Sans&display=swap" rel="stylesheet">
+  <link href="http://code.ionicframework.com/ionicons/1.5.2/css/ionicons.min.css" rel="stylesheet">
+  <script src="//code.jquery.com/jquery-2.1.0.min.js" type="text/javascript"></script>
+  <!-- <link rel="stylesheet" href="css/style-home.css">
     <link rel="stylesheet" href="css/style-faca-parte.css">
-    <link rel="stylesheet" href="css/style-pagina-usuario.css">
-    <!-- <link rel="stylesheet" href="css/timeline.css"> -->
-    <link rel="stylesheet" href="css/style-hfs.css">
+    <link rel="stylesheet" href="css/style-pagina-usuario.css"> -->
+  <!-- <link rel="stylesheet" href="css/timeline.css"> -->
+  <link rel="stylesheet" href="css/login.css">
+  <link rel="stylesheet" href="css/style-hfs.css">
 </head>
 
 <body>
@@ -22,7 +26,8 @@
 
     <nav class="navbar navbar-expand-lg navbar-light">
       <a class="navbar-brand" href="#">Colaki</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado"
+        aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
         <span class="navbar-toggler-icon"></span>
       </button>
 
