1258d5117d37a4644c1fa423f0e49d74e66d2b5c
Carlos1br <eicarlos@outlook.com.br>
2019-11-21 18:57:02 -0300

    Correcao responsividade Header/ pagina login responsiva v1

--- login.php
+++ login.php
@@ -1,30 +1,34 @@
 <?php require_once("header.php");?>
-
-<section class="row" id="section-casas-login">
-    <div class="col-6 casas-login">
-    </div>
-    <div class="col-6">
-        <div class="logon">
-            <h1 class="h4">Faça seu login</h1>
-            <form action="" method="get">
-                <label for="user">
-                    Username ou email <br>
-                    <input type="text" id="user">
-                </label><br>
-                <label for="password">
-                    senha <br>
-                    <input type="text" id="password">
-                    <p><a href="#">Esqueci minha senha</a></p>
-                </label>
-                <h5>Ou entre com:</h5>
-            </form>
-            <span class="logon-with">
-                <img src="images/google.svg">
-                <img src="images/facebook.svg">
-            </span>
+<div class="container-fluid">
+    <section class="row">
+        <div class=" col-sm-6 col-md-6 m-0 p-0">
+            <section class="casas-login"></section>
+            <!-- <img src="images/apartamentos.jpg" class="" > -->
         </div>
-    </div>
+        <div class=" col-sm-6 col-md-6 m-0 p-0">
+            <div class="logon">
+                <h1 class="h4">Faça seu login</h1>
+                <form action="" method="get">
+                    <label for="user">
+                        Username ou email <br>
+                        <input type="text" id="user">
+                    </label><br>
+                    <label for="password">
+                        senha <br>
+                        <input type="text" id="password">
+                        <p><a href="#">Esqueci minha senha</a></p>
+                    </label>
+                    <h5>Ou entre com:</h5>
+                </form>
+                <span class="logon-with">
+                    <img src="images/google.svg">
+                    <img src="images/facebook.svg">
+                </span>
+            </div>
+        </div>
 
-</section>
+    </section>
+</div>
+
 
 <?php require_once("footer.php");?>
